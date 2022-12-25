14ba56a97fc20e2ab673b546bc04c14306c5cca7
Yale Li <lyl921101@gmail.com>
2022-12-24 18:47:37 -0600

    add: 189 rotate array

---
+++ Array/189. Rotate Array.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def rotate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        def reverse(head, tail):
+            while head < tail:
+                nums[head], nums[tail] = nums[tail], nums[head]
+                head += 1
+                tail -= 1
+
+        k = k % len(nums)
+
+        reverse(0, len(nums) - 1 - k)
+        reverse(len(nums) - k, len(nums) - 1)
+        reverse(0, len(nums) - 1)
+        
+        return nums
